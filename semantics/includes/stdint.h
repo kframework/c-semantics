a5c15837b1785fd07d02560454f4aa2ef4eec4c3
Chucky Ellison <cmelliso@gmail.com>
2011-02-21 04:41:49 +0000

    improvements to stdin and printf.  This will probably fail to run due to changes in K

--- semantics/includes/stdint.h
+++ semantics/includes/stdint.h
@@ -1,8 +1,23 @@
 typedef signed char int8_t;
 typedef unsigned char uint8_t;
+# define INT8_MIN		(-128)
+# define INT8_MAX		(127)
+# define UINT8_MAX		(255)
+
 typedef short int16_t;
 typedef unsigned short uint16_t;
+# define INT16_MIN		(-32767-1)
+# define INT16_MAX		(32767)
+# define UINT16_MAX		(65535)
+
 typedef int int32_t;
 typedef unsigned int uint32_t;
+# define INT32_MIN		(-2147483647-1)
+# define INT32_MAX		(2147483647)
+# define UINT32_MAX		(4294967295U)
+
 typedef long long int64_t;
 typedef unsigned long long uint64_t;
+# define INT64_MIN		(-9223372036854775807LL-1)
+# define INT64_MAX		(9223372036854775807LL)
+# define UINT64_MAX		(18446744073709551615ULL)
