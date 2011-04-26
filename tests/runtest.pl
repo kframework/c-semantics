9f0ad2146cfbde6eb5c84064d6d629153f6bce18
Chucky Ellison <cmelliso@gmail.com>
2011-04-26 00:31:49 +0000

    fixing a few problems with new type stuff

--- tests/runtest.pl
+++ tests/runtest.pl
@@ -17,7 +17,7 @@ if ($numArgs != 2) {
 my $kcc = "../dist/kcc";
 #my $kcc = "ccomp -fbitfields -flonglong -fstruct-passing -fstruct-assign -fvararg-calls -dparse -dc";
 #my $kcc = "ccomp -fbitfields -flonglong -fstruct-passing -fstruct-assign -fvararg-calls";
-my $gcc = "gcc -lm -Wall -Wextra -x c -O0 -U __GNUC__ -pedantic -std=c99";
+my $gcc = "gcc -lm -Wall -Wextra -x c -O0 -m32 -U __GNUC__ -pedantic -std=c99";
 
 my $flag = $ARGV[0];
 my $shouldFail;
