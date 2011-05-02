697dc15c153efc3392a242c6da67c6eeae1588c0
Chucky Ellison <cmelliso@gmail.com>
2011-05-02 19:45:12 +0000

    adding required script

---
+++ scripts/slurpFrontEnd.pl
@@ -0,0 +1,1 @@
+use strict;use File::Basename;use File::Spec::Functions qw(rel2abs catfile);my $myDirectory = dirname(rel2abs($0));my $slurpScript = catfile($myDirectory, 'slurp.pl');require $slurpScript;my $input = <>;chomp($input);print slurp($input);
