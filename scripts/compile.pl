1bd72b63cb57b574682bfd127fe5c6c0679495d0
Chris Hathhorn <chathhorn@gmail.com>
2013-06-24 15:29:18 -0500

    Concatenated link script with the compile script. Trying to consolidate and reduce some of the excess sciptage.

--- scripts/compile.pl
+++ scripts/compile.pl
@@ -7,10 +7,6 @@ use File::Temp qw/ tempfile tempdir /;
 use File::Copy;
 
 my $myDirectory = dirname(rel2abs($0));
-#my $slurpScript = catfile($myDirectory, 'slurp.pl');
-my $linkScript = catfile($myDirectory, 'link.pl');
-#require $slurpScript;
-require $linkScript;
 
 my @temporaryFiles = ();
  
@@ -46,7 +42,6 @@ sub finalCleanup {
 		unlink ($file);
 	}
 }
- 
  
 $::VERSION="0.1";
 #use vars qw/$not $re/;
@@ -271,6 +266,56 @@ sub compileProgram {
 		print `dprofpp`;
 	}
 	return 1;
+}
+
+my @programNames;
+my @programs;
+
+sub linker {
+	my @files = (@_);
+	my @operators;
+	my $retval = "";
+	if (scalar(@files) == 0) {
+		die "No files passed to linker\n";
+	}
+	foreach my $filename (@files) {
+		#print "$filename\n";
+		my @contents;
+		if ($filename =~ m/\.a$/) {
+			@contents = `ar -p $filename`;
+			if ($?) {
+				die "Something went wrong when trying to unarchive $filename\n";
+			}
+		} else {
+			open(my $newFile, $filename) or die "Couldn't open file $filename\n";
+			@contents = <$newFile>;
+		}
+		linkFile(@contents);
+	}
+
+	$retval .= "'Program(";
+	$retval .= "'klist(_`(_`)(KList2KLabel_(";
+	$retval .= printNested(@programs);
+	$retval .= '), .KList))';
+	$retval .= ')';
+	return $retval;
+}
+
+sub linkFile {
+	my (@contents) = (@_);
+	foreach my $line (@contents){
+            push(@programs, $line);
+	}
+}
+
+sub printNested {
+	my ($name, @rest) = (@_);
+
+	if (defined($name)) {
+		return "_`,`,_(($name), " . printNested(@rest) .")";
+	} else {
+		return '.KList';
+	}
 }
 
 __END__
