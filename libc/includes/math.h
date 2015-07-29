830955c2e7543a4d2b693301a2baacbd84911445
Edgar Pek <edgar.pek@gmail.com>
2015-07-29 12:26:47 -0500

    added math functions to support OpenPilot specification / verification

--- libc/includes/math.h
+++ libc/includes/math.h
@@ -18,4 +18,13 @@ double fmod(double x, double y);
 double atan2(double y, double x);
 double asin(double x);
 
+// added by Edgar Pek (2015-7-28) for OpenPilot
+float fabsf(float x);
+float sinf(float x);
+float asinf(float x);
+float cosf(float x);
+float tanf(float x);
+float atan2f(float x, float y);
+// -----------------------------------------------
+
 #endif
