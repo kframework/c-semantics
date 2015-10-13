a928c30807fd314d3fb3e2e945f84ba1b376e39e
Dwight Guth <dwightguth@runtimeverification.com>
2015-10-13 13:50:52 -0500

    fix a few bugs

--- tests/unit-pass/pointer-align.c
+++ tests/unit-pass/pointer-align.c
@@ -1,3 +1,4 @@
+#include<stddef.h>
 typedef struct ASN1_TEMPLATE_st ASN1_TEMPLATE;
 struct ASN1_ITEM_st {
    char itype;
@@ -20,7 +21,7 @@ typedef struct ec_privatekey_st {
     char *publicKey;
 } EC_PRIVATEKEY;
 typedef struct ASN1_ITEM_st ASN1_ITEM;
-extern const ASN1_ITEM LONG_it;
+const ASN1_ITEM LONG_it;
 typedef const ASN1_ITEM ASN1_ITEM_EXP;
 struct ASN1_TEMPLATE_st {
     unsigned long flags;
@@ -32,7 +33,7 @@ struct ASN1_TEMPLATE_st {
     ASN1_ITEM_EXP *item;
 };
 static const ASN1_TEMPLATE EC_PRIVATEKEY_seq_tt[] = {
-        { (0), (0), __kcc_offsetof(EC_PRIVATEKEY, version), "version", (&(LONG_it)) },
+        { (0), (0), offsetof(EC_PRIVATEKEY, version), "version", (&(LONG_it)) },
 } ; 
 static const ASN1_ITEM EC_PRIVATEKEY_it = { 0x1, 16, EC_PRIVATEKEY_seq_tt, sizeof(EC_PRIVATEKEY_seq_tt) / sizeof(ASN1_TEMPLATE), ((void *)0), sizeof(EC_PRIVATEKEY), "EC_PRIVATEKEY" };
 
