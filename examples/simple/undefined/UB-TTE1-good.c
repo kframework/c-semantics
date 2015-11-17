3520fd922e2d36f2deed9356b1878f5e4be3763c
chathhorn <hathhorn@gmail.com>
2015-11-17 20:48:11 +0000

    Fix issue with VLAs in sizeof.
    Fixes #140.

---
+++ examples/simple/undefined/UB-TTE1-good.c
@@ -0,0 +1,8 @@
+// Copyright (c) 2015 Runtime Verification, Inc. (RV-Match team). All Rights Reserved.
+
+#include <stddef.h>
+
+int main() {
+      sizeof(0 ? (int (*) [5]) NULL : NULL);
+      return 0;
+}
--- tests/unit-pass/j016.c
+++ tests/unit-pass/j016.c
@@ -1,9 +1,12 @@
+#include <stddef.h>
+
 void f(int n, int (*a)[n]) { }
 
 int main(void) {
       int a[5] = {0};
       int (*b)[5] = &a;
       f(5, b);
+      sizeof(0 ? (int (*) [5]) NULL : NULL);
 
       return 0;
 }
