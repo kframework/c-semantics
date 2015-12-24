9cdea1df82e30c59318c375cc76b25298e43a91a
Manasvi Saxena <manasvi.saxena@runtimeverification.com>
2015-12-24 11:13:55 -0600

    more files

---
+++ examples/simple/posix/UB-PTHREAD3-bad.c
@@ -0,0 +1,18 @@
+// Copyright (c) 2015 Runtime Verification, Inc. (RV-Match team). All Rights Reserved.
+
+#include<stdio.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdlib.h>
+
+
+pthread_mutex_t * lock_ptr;
+
+
+int main(void)
+{
+    lock_ptr = malloc(sizeof(pthread_mutex_t));
+    pthread_mutex_destroy(lock_ptr);
+    
+    return 0;
+}
