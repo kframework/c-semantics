45a1cc33fcb88f0759e692438f17f7bf6e2a3772
Dwight Guth <dwightguth@runtimeverification.com>
2015-12-30 11:29:48 -0600

    modifications to examples to make them tested by rv-match

--- examples/error-codes/undefined/UB-CEA6-bad-static.c
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2015 Runtime Verification, Inc. (RV-Match team). All Rights Reserved.
-
-int x;
-enum e {a = 5 + (int)&x};
-int main(void){
-
-}
--- examples/error-codes/undefined/UB-TIN2-bad-static-link1.c
+++ examples/error-codes/undefined/UB-TIN2-bad-static-link1.c
@@ -1,7 +1,7 @@
 // Copyright (c) 2015 Runtime Verification, Inc. (RV-Match team). All Rights Reserved.
 
 #include <stdio.h>
-extern int a;
+int a;
 
 int main() {
   return 0;
