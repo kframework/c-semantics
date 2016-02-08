d161196560d6ac0212ae6768ccb763e5bd8566c6
Dwight Guth <dwightguth@runtimeverification.com>
2016-02-08 17:01:58 -0600

    fix offsetof

---
+++ tests/unit-pass/offsetof-1.c
@@ -0,0 +1,24 @@
+#include<stddef.h>
+#include<stdlib.h>
+
+struct bar {
+  short y;
+};
+
+struct foo {
+  short x[4];
+  struct bar z[4];
+};
+
+int main() {
+  int offset = offsetof(struct foo, x);
+  if (offset != 0)
+    abort();
+  offset = offsetof(struct foo, x[2]);
+  if (offset != 4)
+    abort();
+  offset = offsetof(struct foo, z->y);
+  if (offset != 8)
+    abort();
+  return 0;
+}
