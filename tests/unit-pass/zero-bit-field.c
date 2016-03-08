f522720ece999ec16d6cf7210762f354e9baaec4
chathhorn <hathhorn@gmail.com>
2016-03-08 22:57:59 +0000

    Some test fixes.

--- tests/unit-pass/zero-bit-field.c
+++ tests/unit-pass/zero-bit-field.c
@@ -1,13 +1,13 @@
 #include<stdlib.h>
 struct foo {
-  int x : 5;
+  int x : 1;
   int : 1;
   int : 0;
-  int z : 2;
+  int z : 1;
 };
 
 int main() {
-  if(sizeof(struct foo) != 2)
-    abort();
+  if(sizeof(struct foo) < 2)
+      abort();
   return 0;
 }
