7cb1d49ad467460fb2907994627bc6b29cd08ce4
Jan Tusil <410062@mail.muni.cz>
2017-01-27 08:54:46 +0100

    Enums: use QualID as an enum ID.

--- tests/unit-pass/enum-empty.C
+++ tests/unit-pass/enum-empty.C
@@ -4,10 +4,15 @@ enum E : int {};
 
 E e;
 
+namespace N { enum E : char{}; }
+
 int main() {
 	E f = e;
 	E * g = nullptr;
 
 	if (sizeof(e) != sizeof(int))
+		abort();
+
+	if (sizeof(enum N::E) != sizeof(char))
 		abort();
 }
