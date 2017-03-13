7de6ef2dd3a824d407eecdf1bd925e454bf84f70
Chris Hathhorn <chathhorn@gmail.com>
2017-03-13 18:01:02 -0500

    Restrict refactoring. (#358)
    * Move restrict check from ptr deref to read/write.
    
    * Better restrict check.
    
    * Minor optimizations to the restrict check.
    
    * Update function-call.k

--- examples/error-codes/undefined/UB-ECL4-bad.c
+++ examples/error-codes/undefined/UB-ECL4-bad.c
@@ -1,12 +1,11 @@
 // Copyright (c) 2015 Runtime Verification, Inc. (RV-Match team). All Rights Reserved.
 
-int g(const int* restrict a, int* b) {
-      *b = 1;
-      *a;
+int g(const int* restrict a) {
+      *(int*)a = 1;
       return 0;
 }
 
 int main(void) {
       int a = 5;
-      return g(&a, &a);
+      return g(&a);
 }
