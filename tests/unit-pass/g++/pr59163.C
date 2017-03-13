d9c338b9b44222f302f2e9636badc434884db111
Dwight Guth <dwight.guth@runtimeverification.com>
2017-03-13 12:49:51 -0500

    Revert "overloading operators + default constructor + misc fixes (#397)"
    This reverts commit 1e217ab13c120e51fb6c08064593e16c3a389ba6.

--- tests/unit-pass/g++/pr59163.C
+++ tests/unit-pass/g++/pr59163.C
@@ -1,8 +1,6 @@
 // PR target/59163
 // { dg-do run }
 
-extern "C" void abort(void);
-
 struct A { float a[4]; };
 struct B { int b; A a; };
 
@@ -10,7 +8,7 @@ __attribute__((noinline, noclone)) void
 bar (A &a)
 {
   if (a.a[0] != 36.0f || a.a[1] != 42.0f || a.a[2] != 48.0f || a.a[3] != 54.0f)
-    abort ();
+    __builtin_abort ();
 }
 
 __attribute__((noinline, noclone)) void
