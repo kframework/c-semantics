3f7ce9ada770569570500ffdbaf5997a02a0200a
Jan Tusil <410062@mail.muni.cz>
2017-04-11 11:08:15 +0200

    operator= for enums

--- tests/unit-pass/enum-operators.C
+++ tests/unit-pass/enum-operators.C
@@ -3,9 +3,57 @@ extern "C" void abort();
 
 enum class E { A = 7, B = 9 };
 
-int main() {
+E & hoo() {
+	static E e = E::B;
+	return e;
+}
+
+enum UE { UE_C = 67 };
+
+void foo(E /*e*/) {
+
+}
+
+void assign_to_enum() {
 	E e = E::A;
 	e = E::B;
+	E const f = E::A;
+	E & r = e;
+	r = f;
+	foo(r);
+	foo(hoo() = e);
+	assert((int)hoo() == (int)e);
+}
+
+E volatile & goo() {
+	static E volatile e = E::A;
+	return e;
+}
+
+void assign_to_volatile_enum() {
+	E e = E::A;
+	volatile E g = E::B;
+	E const f = E::A;
+	e = g;
+	e = f;
+	g = e;
+	g = E::A;
+	goo() = f;
+	assert((int)goo() == (int)f);
+}
+
+void assign_to_int() {
+	UE ue = UE_C;
+	int x = 5;
+	x = ue;
+	assert(x == (int)UE_C);
+}
+
+int main() {
+	assign_to_enum();
+	assign_to_volatile_enum();
+	assign_to_int();
+#if 0
 	assert(E::A < E::B);
 	assert((int)e == (int)E::B);
 	E * p_e = &e;
@@ -34,4 +82,5 @@ int main() {
 
 	e = g;
 	assert(e == g);
+#endif
 }
