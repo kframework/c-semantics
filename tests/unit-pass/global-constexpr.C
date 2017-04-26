7db5d186401848f4753f3f8b5cb958195ea1e0b4
Jan Tusil <410062@mail.muni.cz>
2017-04-26 22:12:32 +0200

    Constexpr for globals - 1

--- tests/unit-pass/global-constexpr.C
+++ tests/unit-pass/global-constexpr.C
@@ -1,3 +1,10 @@
-constexpr int a = 1;
-constexpr int b = ::a;
+extern "C" void abort();
 
+constexpr int a = 1;
+const extern int b;
+int c = b;
+constexpr int b = a;
+
+int main() {
+  if (c != 1) abort();
+}
