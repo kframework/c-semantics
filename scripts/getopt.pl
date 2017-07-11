91e282b4de6a77a8f8ed51072b2333cf78892727
dwightguth <dwight.guth@runtimeverification.com>
2017-07-11 10:37:09 -0500

    fix issues with no java pr (#470)

--- scripts/getopt.pl
+++ scripts/getopt.pl
@@ -1,10 +1,14 @@
 use Getopt::Declare;
 
+# generate getopt declare code
 sub encode { return Getopt::Declare->new(shift,-BUILD)->code() || die }
 
 sub classify {}
 
+# read kcc script from stdin
 my $kcc = do { local $/; <STDIN> };
+# look for getopt declare specification in code and replace it with the code that
+# executes the actual parsing
 $kcc =~ s/^=for\s+Getopt::Declare\s*\n(.*?)\n=cut/'$self->{_internal}{usage} = q(
 ' . $1 . '
 );
