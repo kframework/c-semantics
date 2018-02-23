63703aba752c08c42b6f7d79148537c3dec8eb68
Chris Hathhorn <chathhorn@gmail.com>
2018-02-23 06:48:20 -0600

    Fixes #720. (#723)
    * Fixes #720.
    
    * Slightly rework handling of fieldless structs.

--- examples/c/error-codes/undefined/UB-CDT1-bad-static.c
+++ examples/c/error-codes/undefined/UB-CDT1-bad-static.c
@@ -1,8 +1,24 @@
-// Copyright (c) 2015 Runtime Verification, Inc. (RV-Match team). All Rights Reserved.
+// Copyright (c) 2015-2018 Runtime Verification, Inc. (RV-Match team). All Rights Reserved.
 
-struct s {
+struct e {
 };
 
-int main(void){
+struct {
+      short x;
+      struct e y;
+      union { struct e u1; double u2; } z;
+      long w;
+} s;
 
+int main(void){
+      struct e t;
+
+      s.x = 2;
+      s.y = t;
+      s.z.u2 = 2.0;
+      s.z.u2;
+      s.z.u1;
+      s.w = 2;
+
+      return sizeof(s.y);
 }
