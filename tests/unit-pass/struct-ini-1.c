e1592da0089ccf911487408869f18cca2937a7bc
Chris Hathhorn <chathhorn@gmail.com>
2018-03-08 12:51:36 -0600

    Inits, memset performance improvements (#746)
    * Inits, memset performance improvements
    
    * Some refactoring on io, padding.
    
    * Make padding bits unknown on write, not just bytes.
    
    * Fixes.
    
    * Refactor padding, fix some tests.
    
    * More refactoring.
    
    * Initializer test.

--- tests/unit-pass/struct-ini-1.c
+++ tests/unit-pass/struct-ini-1.c
@@ -9,7 +9,10 @@ struct S object = {'X', 8, 9};
 
 int main ()
 {
+  struct S ss[100000] = {{'X', 8, 9}};
   if (object.f1 != 'X' || object.f2[0] != 8 || object.f2[1] != 9)
+    abort ();
+  if (ss[0].f1 != 'X' || ss[0].f2[0] != 8 || ss[0].f2[1] != 9)
     abort ();
   exit (0);
 }
