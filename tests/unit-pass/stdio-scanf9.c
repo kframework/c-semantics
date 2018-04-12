c2abdfb53ce935f02173de3d8a9be0aeb007d753
Chris Hathhorn <chathhorn@gmail.com>
2018-04-12 09:52:55 -0500

    Support for "m" in scanf conversion specs. (#813)
    * Support for "m" in scanf conversion specs.
    
    * Tests.

--- tests/unit-pass/stdio-scanf9.c
+++ tests/unit-pass/stdio-scanf9.c
@@ -7,6 +7,7 @@ main (void)
 {
   int matches;
   char str[10];
+  char * alloced_str;
 
   str[0] = '\0';
   matches = -9;
@@ -17,6 +18,16 @@ main (void)
   if (strcmp (str, "x"))
     abort ();
 
+  matches = -9;
+  matches = sscanf ("x ]", "%m[^] ]", &alloced_str);
+  printf ("Matches = %d, string str = \"%s\".\n", matches, alloced_str);
+  printf ("str should be \"x\".\n");
+
+  if (strcmp (alloced_str, "x"))
+    abort ();
+
+  free(alloced_str);
+
   str[0] = '\0';
   matches = -9;
   matches = sscanf (" ] x", "%[] ]", str);
@@ -25,6 +36,16 @@ main (void)
 
   if (strcmp (str, " ] "))
     abort ();
+
+  matches = -9;
+  matches = sscanf (" ] x", "%m[] ]", &alloced_str);
+  printf ("Matches = %d, string str = \"%s\".\n", matches, alloced_str);
+  printf ("str should be \" ] \".\n");
+
+  if (strcmp (alloced_str, " ] "))
+    abort ();
+
+  free(alloced_str);
 
   return 0;
 }
