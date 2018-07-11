9f0b6113003873b689508d5b18202e5b78d030ae
Chris Hathhorn <chathhorn@gmail.com>
2018-07-11 07:05:38 -0400

    Enable error recovery in regression tests. (#868)
    * Enable error recovery in regression tests.
    
    To better test error recovery and enable testing multiple error reports with a single test program.
    
    * C++ error recovery case.

--- tests/unit-fail/errorInFor.c
+++ tests/unit-fail/errorInFor.c
@@ -2,7 +2,7 @@
 
 int main(void) {
       int buf[5] = {0};
-      for (int i = 5; buf[i] == 0; i++) {
+      for (int i = 5, j = 0; buf[i] == 0 && j != 5; i++, j++) {
             printf("%d\n", i);
       }
 }
