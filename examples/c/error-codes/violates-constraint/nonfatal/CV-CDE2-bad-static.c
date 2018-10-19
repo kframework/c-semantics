7776226bb31f6fc9f7a6eca697e7f5ee334a3a35
Chris Hathhorn <chathhorn@gmail.com>
2018-10-19 14:09:10 -0500

    Add error for out-of-range enum inits. (#941)

---
+++ examples/c/error-codes/violates-constraint/nonfatal/CV-CDE2-bad-static.c
@@ -0,0 +1,9 @@
+// Copyright (c) 2018 Runtime Verification, Inc. (RV-Match team). All Rights Reserved.
+#include <limits.h>
+
+enum e1 {a = 0xffffffff};
+enum e2 {x = INT_MAX, y, z};
+
+int main(void){
+
+}
---
+++ examples/c/error-codes/violates-constraint/nonfatal/CV-CDE2-good.c
@@ -0,0 +1,8 @@
+// Copyright (c) 2018 Runtime Verification, Inc. (RV-Match team). All Rights Reserved.
+#include <limits.h>
+
+enum e {a = INT_MAX};
+
+int main(void){
+
+}
