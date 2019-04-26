871c3fe358fbe3ba70b8b7e37d5d2f28261d2ace
chathhorn <chathhorn@gmail.com>
2019-04-26 16:57:13 -0500

    Test.

--- tests/unit-pass/template-link1.C
+++ tests/unit-pass/template-link1.C
@@ -2,7 +2,22 @@ template<typename> class X;
 
 template <typename T> void foo(class X<T> *t);
 
+template <typename T> T id(T x);
+
+template <typename T> T id2(T x) { return x; }
+
 int main() {
   class X<int> *t = nullptr;
   foo(t);
+
+  const char * b, * a = "abc";
+  b = id(a);
+
+  int y, x = 5;
+  y = id(x);
+
+  float g, f = 1.0;
+  g = id2(f);
+
+  return a != b || x != y || f != g;
 }
