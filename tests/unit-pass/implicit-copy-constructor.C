b0d88c50ef4bb4d38de6980a679c5c10acd6a959
Jan Tusil <jenda.tusil@gmail.com>
2019-04-29 20:44:37 +0200

    Implicit copy constructors (2) (#385)
    * Odr.doWhenUsed
    
    * Implicitly define SMFs only when ODR-used.
    
    * Every testcase should test only one thing.
    
    * Allways define destructor
    
    just a workaround. It should not really be that way.
    
    * implicitly-defined copy constructor
    
    * non-const copy constructor
    
    * test
    
    * Update Makefile
    
    * Update Makefile
    
    * add a missing guard
    
    * Remove -d flag

---
+++ tests/unit-pass/implicit-copy-constructor.C
@@ -0,0 +1,70 @@
+extern "C" void abort();
+#define assert(x) if(!(x)) abort();
+
+// const-version of copy constructors
+
+struct B1 {
+	int x;
+	B1(){}
+	B1(B1 const &b) : x(b.x + 1) {}
+};
+
+struct S1 : B1{
+	int y;
+};
+
+S1 foo1() {
+	S1 s;
+	s.x = 1;
+	s.y = 3;
+	return s;
+}
+
+void test1() {
+	S1 a;
+	a.x = 3;
+	a.y = 7;
+	S1 b = a;
+	assert(b.x == 4);
+	assert(b.y == 7);
+	foo1();
+	// S1 c = foo1(); // does not work yet
+	//assert(c.y == 3);
+}
+
+// non-const version of copy constructors
+struct B2 {
+	int x;
+	B2(){}
+	B2(B2 /*non-const*/ &b) : x(b.x + 1) {}
+};
+
+struct S2 : B2{
+	// implicitly generated:
+	// S2(S2 /*non-const*/ &s) : B2(s), y(s.y) {}
+	int y;
+};
+
+S2 foo2() {
+	S2 s;
+	s.x = 1;
+	s.y = 3;
+	return s;
+}
+
+void test2() {
+	S2 a;
+	a.x = 3;
+	a.y = 7;
+	S2 b = a;
+	assert(b.x == 4);
+	assert(b.y == 7);
+	foo2();
+	// S1 c = foo1(); // does not work yet
+	//assert(c.y == 3);
+}
+
+int main() {
+	test1();
+	test2();
+}
