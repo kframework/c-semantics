29b34e4b99dee009d109de48bfb0e3cb6c57319f
Chris Hathhorn <chathhorn@gmail.com>
2019-05-06 08:10:36 -0500

    Clang type inference (#377)
    * Spit out template specializations.
    
    * Include deduced auto type.
    
    * First pass at removing TypeExpr.
    
    * Suppress C link errors for mangled ids.
    
    * Cleanup.
    
    * Test.

--- tests/unit-pass/template-link2.C
+++ tests/unit-pass/template-link2.C
@@ -3,3 +3,8 @@ template<typename T> class X {};
 template <typename T> void foo(class X<T> *t) {}
 
 template void foo<int>(class X<int> *t);
+
+template <typename T> T id(T x) { return x; }
+
+template const char * id<const char *>(const char * t);
+template int id<int>(int t);
