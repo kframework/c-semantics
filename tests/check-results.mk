7a0cca231471db5ea4d183b769dd7af88be7f6d2
chathhorn <chathhorn@gmail.com>
2019-07-13 08:31:08 -0500

    Send verbose output to file.

--- tests/check-results.mk
+++ tests/check-results.mk
@@ -2,4 +2,4 @@
 
 CHECK_RESULT_COMPILE = if [ $$? -eq 0 ] ; then echo "passed $<"; mv $@.tmp.out $@.out; else echo "failed $<"; cat $@.tmp.out; exit 1; fi
 CHECK_RESULT_RUN = if [ $$? -eq 0 ] ; then echo "passed $<"; mv $@.tmp $@; else echo "failed $<"; cat $@.tmp; exit 1; fi
-CHECK_RESULT_RUN_VERBOSE = if [ $$? -eq 0 ] ; then echo "passed $<"; mv $@.tmp $@; else echo "failed $<"; cat $@.tmp; env VERBOSE=1 ./$<; exit 1; fi
+CHECK_RESULT_RUN_VERBOSE = if [ $$? -eq 0 ] ; then echo "passed $<"; mv $@.tmp $@; else echo "failed $<"; cat $@.tmp; env VERBOSE=1 ./$< > $@.log 2>&1; exit 1; fi
