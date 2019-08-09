395b71f1881fbec1741ef4f13f18d14626347dcb
chathhorn <chathhorn@gmail.com>
2019-08-09 14:57:29 -0500

    Example for new error code.

---
+++ examples/c/error-codes/undefined/UB-CTYPE1-bad.c
@@ -0,0 +1,18 @@
+#include <ctype.h>
+
+int main() {
+      isalnum(256);
+      isalpha(256);
+      isblank(256);
+      iscntrl(256);
+      isdigit(256);
+      isgraph(256);
+      islower(256);
+      isprint(256);
+      ispunct(256);
+      isspace(256);
+      isupper(256);
+      isxdigit(256);
+      tolower(256);
+      toupper(256);
+}
