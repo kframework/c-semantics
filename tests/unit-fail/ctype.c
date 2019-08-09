e3ffc9eca8f293407c7aa2ec2bc7bc6fbf9cca7d
chathhorn <chathhorn@gmail.com>
2019-08-09 09:52:40 -0500

    Test for ctype char class funs.

---
+++ tests/unit-fail/ctype.c
@@ -0,0 +1,22 @@
+#include <ctype.h>
+#include <stdio.h>
+
+int main() {
+      for (int c = -2; c != 257; ++c) {
+            unsigned x = 0;
+
+            x |= (_Bool) isalnum(c)  << 0;
+            x |= (_Bool) isalpha(c)  << 1;
+            x |= (_Bool) iscntrl(c)  << 2;
+            x |= (_Bool) isdigit(c)  << 3;
+            x |= (_Bool) isgraph(c)  << 4;
+            x |= (_Bool) islower(c)  << 5;
+            x |= (_Bool) isprint(c)  << 6;
+            x |= (_Bool) ispunct(c)  << 7;
+            x |= (_Bool) isspace(c)  << 8;
+            x |= (_Bool) isupper(c)  << 9;
+            x |= (_Bool) isxdigit(c) << 10;
+
+            printf("%d (%x)\n", c, x);
+      }
+}
