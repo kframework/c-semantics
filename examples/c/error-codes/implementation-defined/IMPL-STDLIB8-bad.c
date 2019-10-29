9173de7d03d5a2384eed8d7d3baa13743408d0e0
Chris Hathhorn <chathhorn@gmail.com>
2019-10-29 03:33:20 -0500

    Some C18-related updates (#592)
    * C18.md: C18 changelog.
    
    * Check for zero size arg to *alloc.
    
    * Remove type qualifiers in typeof cast.
    
    * Remove UB-STDLIB7 (aligned_alloc invalid args).
    
    * DR 481 semantics for _Generic.
    
    * Type of rvalues.
    
    * IMPL-STDLIB8 citations, tests.
    
    * Tweak
    
    * Newline.

---
+++ examples/c/error-codes/implementation-defined/IMPL-STDLIB8-bad.c
@@ -0,0 +1,5 @@
+#include <stdlib.h>
+
+int main(void) {
+      char* s = aligned_alloc(8, 0);
+}
--- examples/c/error-codes/undefined/UB-STDLIB7-bad.c
+++
@@ -1,7 +0,0 @@
-#include <stdlib.h>
-
-int main(void) {
-      char* s = aligned_alloc(8, 15);
-      s[2] = 0;
-      return s[2];
-}
